3d4242bac7e7b88a3b467d8f7b5b25417493ae70
Olivia <oliviarodrigue@gmail.com>
2019-04-03 21:06:17 +0200

    Initial Commit

---
+++ Pushor/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Pushor
+//
+//  Created by olivia rodrigue on 03/04/2019.
+//  Copyright © 2019 olivia rodrigue. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
